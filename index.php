f95663445a1e2f71230e33388f7cbb6c810e5f28
Kaysier De los Reyes <69897182+kyzier23@users.noreply.github.com>
2020-10-03 17:41:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<html>
+	<head>
+		<title>HOME</title>
+		<link rel="stylesheet" type = "text/css" href = "css/style.css"/>
+	</head>
+	<body>
+			<div class = "navi">
+			<a href = "index.html"><font color = "black"> HOME</font></a>
+			<a href = "01.html"><font color = "black"> ABOUT US</font></a>
+			<a href = "02.html"><font color = "black"> PORTFOLIO</font></a>
+			<a href = "03.html"><font color = "black"> GALLERY</font></a>
+			<a href = "04.html"><font color = "black"> CLIENTS</font></a>
+			<a href = "05.html"><font color = "black"> CONTACT US</font></a>
+			</div>
+			<br>
+			<br>
+			<div class = "body1">
+			<div class = "content">
+			<br>
+			<h1 align = "center"> HOME</h1>
+			<p align = "center"><font size = "4">We’re not just passionate purveyors of coffee, but everything else that goes with a full and rewarding coffeehouse experience. We also offer a selection of fine pastries and other delectable treats to please the taste buds.
+			</font></p>
+			<p align = "center"><font size = "4">And the music you hear in store is chosen for its artistry and appeal.It’s not unusual to see people coming to JAVA to chat, meet up or even work. We’re a neighborhood gathering place, a part of the daily routine – and we couldn’t be happier about it. 
+			</font></p>
+			<p align = "center"><font size = "4">Get to know us and you’ll see: we are so much more than what we brew. We make sure everything we do is through the lens of humanity – from our commitment to the highest quality coffee in the world, to the way we engage with our customers and communities to do business responsibly.
+			</font></p>
+			</div>
+			<div class = "clear">
+			</div>
+			</div>
+			<br>
+			<div class = "body2">
+			<div class = "left">
+			<h1><font size = "5"> NEWS AND EVENTS</font><h1>
+			<p align = "left" ><font size = "4" face = "Times New Roman"> February 1, 2019</font></p>
+			<p align = "center" ><font size = "4" > JAVA CELEBRATES VALENTINE'S</font></p>
+			<p align = "center" ><font size = "3" face = "Times New Roman" > To kick off the love month this February, JAVA prepared promos and discount for customers visiting our store starting February 1-14, 2019</font></p>
+			<p align = "left" ><font size = "4" face = "Times New Roman"> January 31, 2018</font></p>
+			<p align = "center" ><font size = "4" > JAVA 2018: MEMORIES</font></p>
+			<p align = "center" ><font size = "3" face = "Times New Roman" > A new scent, Charity event 2018, Brewing sessions..<br>
+			See the photos that reminds us of 2018 </font></p>
+			</div>
+			<div class = "clear"></div>
+			</div>
+			<div class = "footer"> 
+			<br>
+			<p><font size = "4"> JAVA </font></p>
+			<p><font size = "2">© 2019 JAVA Coffee Company. All rights reserved. </font></p>
+			
+			</div>
+		
+	</body>
+</html>
